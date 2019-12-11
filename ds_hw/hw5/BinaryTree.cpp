8c5f98f7d29cfa41c260dd167c922cc1d1bb8a4b
吴杰 <wujie@wujiedeMacBook-Pro.local>
2019-12-11 16:22:42 +0800

    updata_hw5

---
+++ ds_hw/hw5/BinaryTree.cpp
@@ -0,0 +1,12 @@
+#include "BinaryTree.templates.h"
+
+template class BinaryTree<int>;
+
+template class BinaryTree<long>;
+
+template class BinaryTree<float>;
+
+template class BinaryTree<double>;
+
+template class BinaryTree<char>;
+
